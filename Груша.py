aea9e301f9c387db8c1899be6a05366a34ed6a91
DmitrenkoSasha <dmitrenko.am@phystech.edu>
2021-12-09 23:39:18 +0300

    Изображается статическая картинка и груша не как одно целое

--- Груша.py
+++ Груша.py
@@ -1,4 +1,9 @@
 import pygame
+import pymunk
+from pymunk.vec2d import Vec2d
+
+#space = pymunk.Space()
+#space.gravity = (0.0, 900.0)
 '''import inspect
 import math
 
@@ -26,17 +31,84 @@ pear_rect = pear.get_rect(bottomright=(400, 300))
 screen.blit(pear, pear_rect)
 
 pygame.display.update()'''
-print(1)  # Проверка на то, что Тренажёрный зал выполняет всё, что написано в этом модул
+print(1)  # Проверка на то, что Тренажёрный зал выполняет всё, что написано в этом модуле
 
 class Pear(pygame.sprite.Sprite):
-    def __init__(self, x, filename):
+    def __init__(self, space, x, filename):
         """Боксёрская груша, пользователь будет ставить её представителей, куда ему захочется,
          и возможно, что будут разные картинки.
          filename: название файла-картинки груши"""
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(filename).convert_alpha()  # Графическое представление спрайта
         self.rect = self.image.get_rect(center = (x, 200))  # Положение и размер спрайта
+        self.space = space
+        self.box_offset = 0, 0
+        self.b1 = self.add_lever(space, (550, 100))
+        self.b2 = self.add_ball(space, (650, 0))
+        c: pymunk.Constraint = pymunk.PinJoint(self.b1, self.b2, (0, 60), (-20, 0))
+        space.add(pymunk.PivotJoint(self.b2, space.static_body, (650, 0) + Vec2d(
+            *self.box_offset)))  # Связывает точку вращения груши и шарик, к которому груша привешена
+        space.add(c)
 
+    def add_ball(self, space, pos):
+        body = pymunk.Body()
+        body.position = Vec2d(*pos) + self.box_offset
+        shape = pymunk.Circle(body, 20)
+        shape.mass = 1
+        shape.friction = 0.7
+        space.add(body, shape)
+        return body
+
+    def add_lever(self, space, pos):
+        body = pymunk.Body()
+        body.position = pos + Vec2d(*self.box_offset) + (0, -20)
+        shape = pymunk.Segment(body, (0, 60), (0, -60), 50)
+        shape.mass = 1
+        shape.friction = 0.7
+        space.add(body, shape)
+        return body
+
+    def update(self, *args):
+        pass
+
+    def smth(self):
+        '''b1 = self.add_lever(space, (550, 100))
+        b2 = self.add_ball(space, (650, 0))
+        c: pymunk.Constraint = pymunk.PinJoint(b1, b2, (0, 60), (-20, 0))
+        space.add(pymunk.PivotJoint(b2, space.static_body, (650, 0) + Vec2d(*self.box_offset)))  # Связывает точку вращения груши и шарик, к которому груша привешена
+        #txts[box_offset] = inspect.getdoc(c)
+        space.add(c)'''
+
+    def check_event_pear(self, event):
+        mouse_joint = None
+        mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if mouse_joint is not None:
+                self.space.remove(mouse_joint)
+                mouse_joint = None
+
+            p = Vec2d(*event.pos)
+            hit = self.space.point_query_nearest(p, 5, pymunk.ShapeFilter())
+            if hit is not None and hit.shape.body.body_type == pymunk.Body.DYNAMIC:
+                shape = hit.shape
+                # Use the closest point on the surface if the click is outside
+                # of the shape.
+                if hit.distance > 0:
+                    nearest = hit.point
+                else:
+                    nearest = p
+                mouse_joint = pymunk.PivotJoint(
+                    mouse_body, shape.body, (0, 0), shape.body.world_to_local(nearest))
+                mouse_joint.max_force = 50000
+                mouse_joint.error_bias = (1 - 0.15) ** 60
+                self.space.add(mouse_joint)
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.b1.apply_impulse_at_local_point((2000, 0))
+            if mouse_joint is not None:
+                self.space.remove(mouse_joint)
+                mouse_joint = None
 '''
 def add_ball(space, pos, box_offset):
     body = pymunk.Body()
@@ -59,14 +131,8 @@ def add_lever(space, pos, box_offset):
 
 
 #txts = {}
+'''
 
-box_offset = 0, 0
-b1 = add_lever(space, (550, 100), box_offset)
-b2 = add_ball(space, (650, 0), box_offset)
-c: pymunk.Constraint = pymunk.PinJoint(b1, b2, (0, 60), (-20, 0))
-space.add(pymunk.PivotJoint(b2, space.static_body, (650, 0) + Vec2d(*box_offset)))  # Связывает точку вращения груши и шарик, к которому груша привешена
-#txts[box_offset] = inspect.getdoc(c)
-space.add(c)
 
 
 mouse_joint = None
@@ -75,44 +141,9 @@ mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 
 #pymunk.lib.cpBodyApplyForceAtWorldPoint(b1, [20, 20], [550, 100])
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if mouse_joint is not None:
-                space.remove(mouse_joint)
-                mouse_joint = None
-
-            p = Vec2d(*event.pos)
-            hit = space.point_query_nearest(p, 5, pymunk.ShapeFilter())
-            if hit is not None and hit.shape.body.body_type == pymunk.Body.DYNAMIC:
-                shape = hit.shape
-                # Use the closest point on the surface if the click is outside
-                # of the shape.
-                if hit.distance > 0:
-                    nearest = hit.point
-                else:
-                    nearest = p
-                mouse_joint = pymunk.PivotJoint(
-                    mouse_body, shape.body, (0, 0), shape.body.world_to_local(nearest)
-                )
-                mouse_joint.max_force = 50000
-                mouse_joint.error_bias = (1 - 0.15) ** 60
-                space.add(mouse_joint)
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            b1.apply_impulse_at_local_point((2000, 0))
-            if mouse_joint is not None:
-                space.remove(mouse_joint)
-                mouse_joint = None
-
-    screen.fill(pygame.Color("white"))
 
     #screen.blit(screen, circle(screen, (0,110,0), (b1.position[0]-270,b1.position[1]-0), 100))
-
+'''
     mouse_pos = pygame.mouse.get_pos()
 
 
@@ -121,7 +152,7 @@ while True:
     space.step(1.0 / 60)
 
     space.debug_draw(draw_options)
-    pygame.display.flip()
+    pygame.display.flip() # Обновляет весь экран
 
     clock.tick(60)
     pygame.display.set_caption(f"fps: {clock.get_fps()}")'''
--- Тренажерный зал.py
+++ Тренажерный зал.py
@@ -1,5 +1,5 @@
 import pymunk.pygame_util
-import pygame
+#import pygame
 from human import *
 from Груша import Pear
 
@@ -10,6 +10,7 @@ pygame.init()
 W = 1000
 H = 600
 screen = pygame.display.set_mode((W, H))
+space = pymunk.Space()
 space.gravity = (0, 100)  # По горизонтали 0, по вертикали 500 в вымышленных единицах
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("Arial", 16)
@@ -31,20 +32,31 @@ def walls():
 
 create_Human()
 walls()
-p1 = Pear(W//2, 'боксёрская груша.jpg')
+
+items = pygame.sprite.Group()
+p1 = Pear(space, W//2, 'боксёрская груша.jpg')
+items.add(p1)
+
+
+mouse_joint = None
+mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 
 while alive:
     for event in pygame.event.get():
+        p1.check_event_pear(event)
         check_event_human(event)
+
         if event.type == pygame.QUIT:
+            alive = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             alive = False
 
 
     screen.fill(WHITE)
-    screen.blit(p1.image, p1.rect)
+    items.draw(screen)
     space.step(1 / 50)  # Независимый цикл пересчитывающий физику
     space.debug_draw(draw_options)
-    pygame.display.update()
+    pygame.display.update()  # Обновляет весь экран, если не передать аргумент
 
     clock.tick(30)
 
