17c233d04c480da2c7598efdc6d8d1153df5be96
DmitrenkoSasha <dmitrenko.am@phystech.edu>
2021-12-12 17:11:08 +0300

    Изменения, связанные с созданием класса Human

--- Тренажерный зал.py
+++ Тренажерный зал.py
@@ -22,7 +22,7 @@ logos: List[pymunk.Shape] = []
 def work_with_items(items):
     for item in items:
         if type(item) is Pear:
-            rotated_logo_img, p, ps = item.work_with_logo()
+            rotated_logo_img, p, ps = item.rotate()
             screen.blit(rotated_logo_img, (round(p.x), round(p.y)))
             pygame.draw.lines(screen, pygame.Color("red"), False, ps, 1)
 
@@ -39,15 +39,14 @@ def walls():
     roof_shape = pymunk.Segment(space.static_body, (0, 0), (1000, 0), 50)
     space.add(roof_shape)
 
-create_Human()
+h1 = Human(space)
+h1.create_Human()
 walls()
 
 items = pygame.sprite.Group()
-p1 = Pear(space, W//2, 'боксёрская груша.jpg')
-items.add(p1)
-logos.append(p1.shape)
-
-
+p1 = Pear(space, W//3, H//2, 'боксёрская груша.jpg')
+#items.add(p1)
+#logos.append(p1.shape)
 
 
 mouse_joint = None
@@ -56,18 +55,20 @@ mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 while alive:
 
     for event in pygame.event.get():
-        p1.check_event_pear(event)
-        check_event_human(event)
+        #p1.check_event_pear(event)
+        h1.check_event_human(event)
 
         if event.type == pygame.QUIT:
             alive = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             alive = False
+        elif event.type == pygame.MOUSEMOTION:
+            mouse_pos = pymunk.pygame_util.get_mouse_pos(screen)
 
 
     screen.fill(WHITE)
-    work_with_items(items)
-    items.draw(screen)
+    #work_with_items(items)
+    #items.draw(screen)
     space.step(1 / 50)  # Независимый цикл пересчитывающий физику
     space.debug_draw(draw_options)
     pygame.display.update()  # Обновляет весь экран, если не передать аргумент
