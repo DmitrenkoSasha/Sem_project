b0229fac333ebf552ec5a1c214097f84504f3257
Krivchuk Vasily <krivchuk.vo@phystech.edu>
2021-12-16 19:08:37 +0300

    Исправлен баг со столкновениями человеков

--- battle_zone.py
+++ battle_zone.py
@@ -76,27 +76,30 @@ def main_battle(number_of_room):
             Returns:
                 collision: [bool] - Булеан, обозначающий, необходимо ли обрабатывать столкновение
         """
-        for c in arbiter.contact_point_set.points:
-            r = max(3, abs(c.distance * 5))
-            r = int(r)
+        part_1 = arbiter.shapes[0]
+        part_2 = arbiter.shapes[1]
+        if ((part_1 in human_1.shapes) and (part_2 in human_2.shapes)) or ((part_2 in human_1.shapes) and (part_1 in human_2.shapes)):
+            for c in arbiter.contact_point_set.points:
+                r = max(3, abs(c.distance * 5))
+                r = int(r)
 
-            p = pymunk.pygame_util.to_pygame(c.point_a, data["surface"])
-            pygame.draw.circle(data["surface"], pygame.Color("black"), p, r, 1)
-            for i in range(100):
-                create_blood(space, p, 2)
-            sound2.play()
+                p = pymunk.pygame_util.to_pygame(c.point_a, data["surface"])
+                pygame.draw.circle(data["surface"], pygame.Color("black"), p, r, 1)
+                for i in range(20):
+                    create_blood(space, p, 2)
+                sound2.play()
 
-            #  Draw stuff
-            balls_to_remove = []
-            for ball in balls:
-                if ball.body.position.y > 700:
-                    balls_to_remove.append(ball)
-                p = tuple(map(int, ball.body.position))
-                pygame.draw.circle(screen, pygame.Color("blue"), p, int(ball.radius), 2)
+                #  Draw stuff
+                balls_to_remove = []
+                for ball in balls:
+                    if ball.body.position.y > 700:
+                        balls_to_remove.append(ball)
+                    p = tuple(map(int, ball.body.position))
+                    pygame.draw.circle(screen, pygame.Color("blue"), p, int(ball.radius), 2)
 
-            for ball in balls_to_remove:
-                space.remove(ball, ball.body)
-                balls.remove(ball)
+                for ball in balls_to_remove:
+                    space.remove(ball, ball.body)
+                    balls.remove(ball)
         return True
 
     def count_points(arbiter, space, data):
@@ -108,49 +111,77 @@ def main_battle(number_of_room):
         """
         part_1 = arbiter.shapes[0]
         part_2 = arbiter.shapes[1]
-        #print(part_1)
-        #print(part_2)
         if part_1 == human_1.shapes[0]:
             if part_2 == human_2.shapes[0]:
                 human_1.points -= 2
                 human_2.points -= 2
-            if part_2 == human_2.shapes[1]:
-                human_1.points -= 1
-            for i in range(2, 6, 1):
-                if part_2 == human_2.shapes[i]:
-                    human_1.points -= 3
-            for i in range(6, 10, 1):
+            for i in range (2, 6, 1):
                 if part_2 == human_2.shapes[i]:
                     human_1.points -= 2
-
+            for i in range(6, 10, 1):
+                if part_2 == human_2.shapes[i]:
+                    human_1.points -= 3
+        elif part_2 == human_1.shapes[0]:
+            if part_1 == human_2.shapes[0]:
+                human_1.points -= 2
+                human_2.points -= 2
+            for i in range (2, 6, 1):
+                if part_1 == human_2.shapes[i]:
+                    human_1.points -= 2
+            for i in range(6, 10, 1):
+                if part_1 == human_2.shapes[i]:
+                    human_1.points -= 3  #head 1 blow
         elif part_1 == human_2.shapes[0]:
             if part_2 == human_1.shapes[0]:
                 human_1.points -= 2
                 human_2.points -= 2
-            if part_2 == human_1.shapes[1]:
-                human_2.points -= 1
-            for i in range(2, 6, 1):
+            for i in range (2, 6, 1):
+                if part_2 == human_1.shapes[i]:
+                    human_2.points -= 2
+            for i in range(6, 10, 1):
                 if part_2 == human_1.shapes[i]:
                     human_2.points -= 3
-            for i in range(6, 10, 1):
-                if part_2 == human_1.shapes[i]:
+        elif part_2 == human_2.shapes[0]:
+            if part_1 == human_1.shapes[0]:
+                human_1.points -= 2
+                human_2.points -= 2
+            for i in range (2, 6, 1):
+                if part_1 == human_1.shapes[i]:
                     human_2.points -= 2
-
-        elif part_1 == human_1.shapes[1]:
-            for i in range(2, 6, 1):
-                if part_2 == human_1.shapes[i]:
-                    human_1.points -= 2
             for i in range(6, 10, 1):
-                if part_2 == human_1.shapes[i]:
+                if part_1 == human_1.shapes[i]:
+                    human_2.points -= 3     #head 2 blow
+
+        if part_1 == human_1.shapes[1]:
+            for i in range (2, 6, 1):
+                if part_2 == human_2.shapes[i]:
                     human_1.points -= 1
+            for i in range(6, 10, 1):
+                if part_2 == human_2.shapes[i]:
+                    human_1.points -= 2
+        elif part_2 == human_1.shapes[1]:
+            for i in range (2, 6, 1):
+                if part_1 == human_2.shapes[i]:
+                    human_1.points -= 1
+            for i in range(6, 10, 1):
+                if part_1 == human_2.shapes[i]:
+                    human_1.points -= 2     #body 1 blow
 
         elif part_1 == human_2.shapes[1]:
-            for i in range(2, 6, 1):
-                if part_2 == human_2.shapes[i]:
-                    human_2.points -= 2
-            for i in range(6, 10, 1):
-                if part_2 == human_2.shapes[i]:
+            for i in range (2, 6, 1):
+                if part_2 == human_1.shapes[i]:
                     human_2.points -= 1
+            for i in range(6, 10, 1):
+                if part_2 == human_1.shapes[i]:
+                    human_2.points -= 2
+        elif part_2 == human_2.shapes[1]:
+            for i in range (2, 6, 1):
+                if part_1 == human_1.shapes[i]:
+                    human_2.points -= 1
+            for i in range(6, 10, 1):
+                if part_1 == human_1.shapes[i]:
+                    human_2.points -= 2    #body 1 blow
+
 
     def add_blood_handler(object_1, object_2):
         """ Добавляет обработчик столкновений между объектами с заданными типами collision_types
@@ -158,44 +189,30 @@ def main_battle(number_of_room):
                 object_1: [int] - первый объект (из collision_types)
                 object_2: [int] - второй объект (из collision_types)
         """
-        handler = space.add_collision_handler(collision_types[object_1], collision_types[object_2])
+        handler = space.add_collision_handler(object_1, object_2)
         handler.data["surface"] = screen
         handler.begin = draw_blood
         handler.separate = count_points
-        handlers.append(handler)
 
     human_1 = Human(space)
-    human_1.create_Human(300, 450, 0)
+    human_1.create_Human(300, 450)
     for shape in human_1.shapes:
         shape.color = pygame.Color('red')
-    for shape in human_1_shapes:
-        print(shape.collision_type)
     human_2 = Human(space)
-    human_2.create_Human(700, 450, 10)
+    human_2.create_Human(700, 450)
     for shape in human_2.shapes:
         shape.color = pygame.Color('green2')
-    for shape in human_1_shapes:
+
+    for shape in human_2.shapes:
         print(shape.collision_type)
+        '''if shape.collision_type == 0:
+            shape.color = pygame.Color('white')'''
+
     room = create_room(space, number_of_room)  # сюда обращаться за нужной комнатой
     room.run()
-
-    add_blood_handler(0, 13)
-    add_blood_handler(0, 15)
-    add_blood_handler(0, 17)
-    add_blood_handler(0, 19)
-    add_blood_handler(1, 13)
-    add_blood_handler(1, 15)
-    add_blood_handler(1, 17)
-    add_blood_handler(1, 19)
-
-    add_blood_handler(10, 3)
-    add_blood_handler(10, 5)
-    add_blood_handler(10, 7)
-    add_blood_handler(10, 9)
-    add_blood_handler(11, 3)
-    add_blood_handler(11, 5)
-    add_blood_handler(11, 7)
-    add_blood_handler(11, 9)
+    add_blood_handler(0, 0)
+    add_blood_handler(0, 3)
+    add_blood_handler(1, 3)
 
     def make_text(points_1, points_2):
         """ Выводит на экран текст с количеством жизней у людей
--- human.py
+++ human.py
@@ -13,7 +13,7 @@ scale = 1
 font = pygame.font.SysFont("Arial", 16)
 options = pymunk.pygame_util.DrawOptions(screen)
 options.flags = pymunk.SpaceDebugDrawOptions.DRAW_SHAPES
-collision_types = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+collision_types = (0, 1, 2, 3)
 ''' "head_1": 0,
     "body_1": 1,
     "right_arm_1": 2,
@@ -74,25 +74,46 @@ class Human:
         self.space.add(body, shape)
         return body
 
-    def create_Human(self, x, y, i):
+    def create_Human(self, x, y):
         """ Создаёт человека поэлементно, с добавлением суставов и пружин
             Args:
                 x: [float] - x-координата человека
                 y: [float] - y-координата человека
                 i: [int] - параметр, отвечающий за определение типов столкновения collision_types
         """
-        head = self.add_lever((x, y), (0, -30 * scale), (0, -32 * scale), 18, 1, 1021, i)
-        telo = self.add_lever((x, y), (0, 30 * scale), (0, -30 * scale), 6, 2, 682, i+1)
+        head = self.add_lever((x, y), (0, -30 * scale), (0, -32 * scale), 18, 1, 1021, 0)
+        telo = self.add_lever((x, y), (0, 30 * scale), (0, -30 * scale), 6, 2, 682, 1)
         c_head_body = pymunk.PivotJoint(head, telo, (x, y - 30 * scale))
         self.space.add(c_head_body)
 
-        right_leg = self.add_lever((x, y), (0, 30 * scale), (20 * scale, 50 * scale), 6, 256, 477, i+6)
-        right_feet = self.add_lever((x, y), (20 * scale, 50 * scale), (20 * scale, 80 * scale), 6, 512, 767, i+7)
+        right_arm = self.add_lever((x, y), (0, -5 * scale), (15 * scale, -20 * scale), 6, 16, 985, 2)
+        right_hand = self.add_lever((x, y), (15 * scale, -20 * scale), (40 * scale, -45 * scale), 6, 32, 991, 3)
+        c_right_arm = pymunk.PivotJoint(right_arm, right_hand, (x + 15 * scale, y - 20 * scale))
+        self.space.add(c_right_arm)
+
+        left_arm = self.add_lever((x, y), (0, -5 * scale), (-15 * scale, -20 * scale), 6, 4, 997, 2)
+        left_hand = self.add_lever((x, y), (-15 * scale, -20 * scale), (-40 * scale, -45 * scale), 6, 8, 1019, 3)
+        c_left_arm = pymunk.PivotJoint(left_arm, left_hand, (x - 15 * scale, y - 20 * scale))
+        self.space.add(c_left_arm)
+
+        c_body_right_arm = pymunk.PivotJoint(right_arm, telo, (x, y - 5 * scale))
+        c_body_left_arm = pymunk.PivotJoint(left_arm, telo, (x, y - 5 * scale))
+        spring_body_right_arm = pymunk.DampedSpring(telo, right_arm, (0, 20 * scale), (20 * scale, -20 * scale),
+                                                    40 * scale, 200, 0.3)
+        spring_body_left_arm = pymunk.DampedSpring(telo, left_arm, (0, 20 * scale), (-20 * scale, -20 * scale),
+                                                   40 * scale, 200, 0.3)
+        self.space.add(c_body_right_arm)
+        self.space.add(c_body_left_arm)
+        self.space.add(spring_body_right_arm)
+        self.space.add(spring_body_left_arm)
+
+        right_leg = self.add_lever((x, y), (0, 30 * scale), (20 * scale, 50 * scale), 6, 256, 477, 2)
+        right_feet = self.add_lever((x, y), (20 * scale, 50 * scale), (20 * scale, 80 * scale), 6, 512, 767, 3)
         c_right_leg = pymunk.PivotJoint(right_leg, right_feet, (x + 20 * scale, y + 50 * scale))
         self.space.add(c_right_leg)
 
-        left_leg = self.add_lever((x, y), (0, 30 * scale), (-20 * scale, 50 * scale), 6, 64, 637, i+8)
-        left_feet = self.add_lever((x, y), (-20 * scale, 50 * scale), (-20 * scale, 80 * scale), 6, 128, 959, i+9)
+        left_leg = self.add_lever((x, y), (0, 30 * scale), (-20 * scale, 50 * scale), 6, 64, 637, 2)
+        left_feet = self.add_lever((x, y), (-20 * scale, 50 * scale), (-20 * scale, 80 * scale), 6, 128, 959, 3)
         c_left_leg = pymunk.PivotJoint(left_leg, left_feet, (x - 20 * scale, y + 50 * scale))
         self.space.add(c_left_leg)
 
@@ -112,27 +133,6 @@ class Human:
         self.space.add(spring_legs_2)
         self.space.add(spring_body_right_leg)
         self.space.add(spring_body_left_leg)
-
-        right_arm = self.add_lever((x, y), (0, -5 * scale), (15 * scale, -20 * scale), 6, 16, 985, i+2)
-        right_hand = self.add_lever((x, y), (15 * scale, -20 * scale), (40 * scale, -45 * scale), 6, 32, 991, i+3)
-        c_right_arm = pymunk.PivotJoint(right_arm, right_hand, (x + 15 * scale, y - 20 * scale))
-        self.space.add(c_right_arm)
-
-        left_arm = self.add_lever((x, y), (0, -5 * scale), (-15 * scale, -20 * scale), 6, 4, 997, i+4)
-        left_hand = self.add_lever((x, y), (-15 * scale, -20 * scale), (-40 * scale, -45 * scale), 6, 8, 1019, i+5)
-        c_left_arm = pymunk.PivotJoint(left_arm, left_hand, (x - 15 * scale, y - 20 * scale))
-        self.space.add(c_left_arm)
-
-        c_body_right_arm = pymunk.PivotJoint(right_arm, telo, (x, y - 5 * scale))
-        c_body_left_arm = pymunk.PivotJoint(left_arm, telo, (x, y - 5 * scale))
-        spring_body_right_arm = pymunk.DampedSpring(telo, right_arm, (0, 20 * scale), (20 * scale, -20 * scale),
-                                                    40 * scale, 200, 0.3)
-        spring_body_left_arm = pymunk.DampedSpring(telo, left_arm, (0, 20 * scale), (-20 * scale, -20 * scale),
-                                                   40 * scale, 200, 0.3)
-        self.space.add(c_body_right_arm)
-        self.space.add(c_body_left_arm)
-        self.space.add(spring_body_right_arm)
-        self.space.add(spring_body_left_arm)
 
         self.complect = ([head, telo, right_arm, left_arm,
                           right_leg, left_leg])
