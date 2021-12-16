dedd93aa785b8de0a02a2c1c836007c42b29f5e7
DmitrenkoSasha <dmitrenko.am@phystech.edu>
2021-12-16 03:36:25 +0300

    Исправление по PEP8.

--- gym.py
+++ gym.py
@@ -8,24 +8,26 @@ import random
 
 from equipment import Pear, Ball
 
-def main_gym():
-    alive = True
-    WHITE = (255, 255, 255)
 
+def main_gym():
     pygame.init()
+    alive = True
+
+    WHITE = (255, 255, 255)
     W = 1000
     H = 700
-    bg = pygame.image.load(r'background.png')
     FPS = 60
 
-    screen = pygame.display.set_mode((W, H))
-    space = pymunk.Space()
-    space.gravity = (0, 900)  # По горизонтали 0, по вертикали 500 в вымышленных единицах
-    space.sleep_time_threshold = 0.3
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("Arial", 16)
-    options = pymunk.pygame_util.DrawOptions(screen)
-    options.flags = pymunk.SpaceDebugDrawOptions.DRAW_SHAPES
+    bg = pygame.image.load(r'background.png')
+
+    screen_gym = pygame.display.set_mode((W, H))
+    space_gym = pymunk.Space()
+    space_gym.gravity = (0, 900)  # По горизонтали 0, по вертикали 500 в вымышленных единицах
+    space_gym.sleep_time_threshold = 0.3
+    clock_gym = pygame.time.Clock()
+    font_gym = pygame.font.SysFont("Arial", 16)
+    options_gym = pymunk.pygame_util.DrawOptions(screen_gym)
+    options_gym.flags = pymunk.SpaceDebugDrawOptions.DRAW_SHAPES
     pymunk.pygame_util.positive_y_is_up = False
 
     (img_x, img_y) = (100, 100)
@@ -34,15 +36,15 @@ def main_gym():
 
     def sample_func(point):
         try:
-            p = pymunk.pygame_util.to_pygame(point, logo_img)
-            color = logo_img.get_at(p)
+            position = pymunk.pygame_util.to_pygame(point, logo_img)
+            color = logo_img.get_at(position)
 
             return color.a
 
         except:
             return 0
-    #def line_around_img(space, filename, img_x, img_y):
-    ### Generate geometry from pymunk logo image
+
+    # Generate geometry from pymunk logo image
 
     logo_img = pygame.image.load(filename).convert_alpha()
     logo_bb = pymunk.BB(0, 0, logo_img.get_width(), logo_img.get_height())
@@ -51,75 +53,62 @@ def main_gym():
     line_set = pymunk.autogeometry.march_soft(logo_bb, logo_img.get_width(), logo_img.get_height(), 99, sample_func)
     logo_img.unlock()
 
-
-
     for line in line_set:
 
         # Returns a copy of a polyline simplified by using the Douglas-Peucker algorithm
         line = pymunk.autogeometry.simplify_curves(line, 0.7)
 
         for i in range(len(line) - 1):
-            shape = pymunk.Segment(space.static_body, line[i]+(img_x, img_y), line[i + 1]+(img_x, img_y), 1)
+            shape = pymunk.Segment(space_gym.static_body, line[i] + (img_x, img_y), line[i + 1] + (img_x, img_y), 1)
             shape.friction = 0.5
             shape.color = (255, 0, 0, 0)
-            space.add(shape)
-
-
+            space_gym.add(shape)
 
     def show_img_things(equipment):
         """Рисует картинки элементов из списка снарядов в зале
         equipment: список снарядов, которые будут видны в зале, и с которыми можно взаимодействовать"""
         for one in equipment:
             if type(one) is Pear:
-                rotated_logo_img, p, ps = one.rotate()
-                screen.blit(rotated_logo_img, (round(p.x), round(p.y)))
-                #pygame.draw.lines(screen, pygame.Color("red"), False, ps, 1)
-
-
+                rotated_logo_img, vec_to_c, ps = one.rotate()
+                screen_gym.blit(rotated_logo_img, (round(vec_to_c.x), round(vec_to_c.y)))
+                #  pygame.draw.lines(screen_gym, pygame.Color("red"), False, ps, 1)
 
     def walls():
-        floor_shape = pymunk.Segment(space.static_body, (0, H), (W, H), 50)
-        space.add(floor_shape)
+        floor_shape = pymunk.Segment(space_gym.static_body, (0, H), (W, H), 50)
+        space_gym.add(floor_shape)
 
-        left_wall_shape = pymunk.Segment(space.static_body, (0, 0), (0, H), 50)
-        space.add(left_wall_shape)
+        left_wall_shape = pymunk.Segment(space_gym.static_body, (0, 0), (0, H), 50)
+        space_gym.add(left_wall_shape)
 
-        right_wall_shape = pymunk.Segment(space.static_body, (W, 0), (W, H), 50)
-        space.add(right_wall_shape)
+        right_wall_shape = pymunk.Segment(space_gym.static_body, (W, 0), (W, H), 50)
+        space_gym.add(right_wall_shape)
 
-        roof_shape = pymunk.Segment(space.static_body, (0, 0), (W, 0), 50)
-        space.add(roof_shape)
-
-    #line_around_img(space, 'мяч.png', 100, 100)
+        roof_shape = pymunk.Segment(space_gym.static_body, (0, 0), (W, 0), 50)
+        space_gym.add(roof_shape)
 
     humans = []
     things = []
-    active_shape = None
     active_thing = None
 
-    h1 = Human(space)
+    h1 = Human(space_gym)
     humans.append(h1)
     h1.create_Human(400, 100)
     walls()
 
-    items = pygame.sprite.Group()
-    p1 = Pear(space, W//3, H//2, 'груша.png')
+    p1 = Pear(space_gym, W // 3, H // 2, 'груша.png')
     things.append(p1)
-    # items.add(p1)
-
 
     mouse_joint = None
     mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 
-
     events = []
-    events.append((5.0, Ball(space, 300, 100).add_ball))
-    events.append((10.0, Ball(space, 300, 100).add_ball))
-    events.sort(key=lambda x: x[0])
+    events.append((5.0, Ball(space_gym, 300, 100).add_ball))
+    events.append((10.0, Ball(space_gym, 300, 100).add_ball))
+    events.sort(key=lambda z: z[0])
     total_time = 0
 
-    SMALLBALL = pygame.USEREVENT + 1
-    pygame.time.set_timer(SMALLBALL, 100)
+    smallball = pygame.USEREVENT + 1
+    pygame.time.set_timer(smallball, 100)
 
     small_balls = 100
 
@@ -130,8 +119,8 @@ def main_gym():
             if event.type == pygame.QUIT:
                 alive = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                p = from_pygame(event.pos, screen)  # Встроенная ф-ция, чтобы переводить коорд. из формата pygame в pymunk
-                active_shape = None
+                #  Встроенная ф-ция, чтобы переводить коорд. из формата pygame в pymunk
+                p = from_pygame(event.pos, screen_gym)
                 active_thing = None
                 for thing in things:
                     if type(thing.shape) is pymunk.shapes.Poly:
@@ -140,12 +129,12 @@ def main_gym():
                             active_thing = thing
 
                 if mouse_joint is not None:
-                    space.remove(mouse_joint)
+                    space_gym.remove(mouse_joint)
                     mouse_joint = None
 
                 p = Vec2d(*event.pos)
 
-                hit = space.point_query_nearest(p, 5, pymunk.ShapeFilter())
+                hit = space_gym.point_query_nearest(p, 5, pymunk.ShapeFilter())
                 if hit is not None and hit.shape.body.body_type == pymunk.Body.DYNAMIC:
                     # Используем ближайшую точку на поверхности в заданном радиусе
                     # если точка нажатия мышки за формой тела
@@ -158,11 +147,11 @@ def main_gym():
                     )
                     mouse_joint.max_force = 50000
                     mouse_joint.error_bias = (1 - 0.15) ** 60
-                    space.add(mouse_joint)
+                    space_gym.add(mouse_joint)
 
-            elif event.type == SMALLBALL:
+            elif event.type == smallball:
                 if small_balls <= 0:
-                    pygame.time.set_timer(SMALLBALL, 0)
+                    pygame.time.set_timer(smallball, 0)
                 for x in range(10):
                     small_balls -= 1
                     mass = 3
@@ -173,22 +162,19 @@ def main_gym():
                     c.friction = 1
                     b.position = random.randint(100, 400), 0
 
-                    space.add(b, c)
+                    space_gym.add(b, c)
 
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 alive = False
             elif event.type == pygame.MOUSEBUTTONUP:
                 if mouse_joint is not None:
-                    space.remove(mouse_joint)
+                    space_gym.remove(mouse_joint)
                     mouse_joint = None
-            elif event.type == pygame.MOUSEMOTION:
-                mouse_pos = pymunk.pygame_util.get_mouse_pos(screen)
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
                 t = active_thing  # Находим тело, на которое нажали мышкой
                 if t is not None:
-                    space.remove(t.shape, t.body)
+                    space_gym.remove(t.shape, t.body)
                     things.remove(t)
-
 
         mouse_pos = pygame.mouse.get_pos()
         mouse_body.position = mouse_pos
@@ -196,23 +182,18 @@ def main_gym():
         if len(events) > 0 and total_time > events[0][0]:
             t, f = events.pop(0)
 
-            f(space)
+            f(space_gym)
 
-        space.step(1 / FPS)  # Независимый цикл пересчитывающий физику
-        #items.draw(screen)
-        screen.fill(WHITE)
-        screen.blit(bg, (0, 0))
-        space.debug_draw(options)
+        space_gym.step(1 / FPS)  # Независимый цикл пересчитывающий физику
+        screen_gym.fill(WHITE)
+        screen_gym.blit(bg, (0, 0))
+        space_gym.debug_draw(options_gym)
         show_img_things(things)
-        screen.blit(pygame.image.load('мяч.png').convert_alpha(), (img_x, img_y))
-
+        screen_gym.blit(pygame.image.load('мяч.png').convert_alpha(), (img_x, img_y))
 
         pygame.display.flip()
 
-        dt = clock.tick(FPS)
+        dt = clock_gym.tick(FPS)
         total_time += dt / 1000
 
-
     pygame.quit()
-if __name__ == "__main__":
-    main_battle(0)
